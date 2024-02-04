a7d8a929a4ce0642e0ea1139f278c7dc5c39ca59
gneiru <noelrohi59@gmail.com>
2024-02-04 15:38:53 +0800

    fmt

--- src/components/typography/h1.tsx
+++ src/components/typography/h1.tsx
@@ -1,6 +1,6 @@
 export function H1({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="scroll-m-20 font-extrabold text-4xl tracking-tight lg:text-5xl">
       {children}
     </h1>
   );
